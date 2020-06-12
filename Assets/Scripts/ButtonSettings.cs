563bdce85e3d1d14a0e2617f451bf2f2b934b4c4
Heloisa <heloisaguimaraes86@gmail.com>
2020-06-12 00:48:44 -0300

    Submetting the project.

---
+++ Assets/Scripts/ButtonSettings.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+
+public class ButtonSettings : MonoBehaviour, IPointerDownHandler, IPointerUpHandler{
+  
+  
+  [SerializeField]
+  private GameObject molecule;
+  [SerializeField]
+  private string id;
+  private Transform moleculePosition;
+  private bool isPressing = false;
+
+
+    void Start(){
+        moleculePosition = molecule.GetComponent <Transform>();
+
+
+
+    }
+
+   
+
+    void Update(){
+        if (isPressing){
+            if(id.Equals("Left")){
+                moleculePosition.position = new Vector3 (-240, moleculePosition.position.y, moleculePosition.position.z);
+            }else if (id.Equals("Right")){
+                moleculePosition.position = new Vector3 (240, moleculePosition.position.y, moleculePosition.position.z);
+            }else {
+                moleculePosition.position = new Vector3 (moleculePosition.position.x, moleculePosition.position.y - 0.1f, moleculePosition.position.z);
+            }
+        }
+        
+        
+    }
+
+
+    public void OnPointerDown(PointerEventData eventData){
+        isPressing = true;
+    
+     }
+    public void OnPointerUp(PointerEventData eventData){
+        isPressing = false;
+     
+     }
+}
+/*
+    public void moveLeft(){
+        //positions.position = new Vector3 (positions.position.x - 0.1f, positions.position.y, positions.position.z);
+    }
+     public void moveRight(){
+       // positions.position = new Vector3 (positions.position.x + 0.1f, positions.position.y, positions.position.z);
+    }
+
+    public void moveDown(){
+        //positions.position = new Vector3 (positions.position.x, positions.position.y - 0.1f, positions.position.z);
+        
+    }*/
---
+++ Assets/Scripts/MoleculeAtlas.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.U2D;
+
+
+
+
+
+public class MoleculeAtlas : MonoBehaviour{
+    
+    [SerializeField]
+    private SpriteAtlas atlas; 
+    private SpriteRenderer myRenderer;
+
+    private Rigidbody2D moleculeRB;//fisica do material
+    private float velocity = -0.5f;
+    
+    private Transform moleculePosition;
+    
+    private float posX = 1.875f;
+
+   
+
+    
+
+
+    void Start(){
+        myRenderer = GetComponent <SpriteRenderer>();
+        moleculeRB = GetComponent <Rigidbody2D>();
+
+        moleculePosition = GetComponent <Transform>();
+        
+
+        changeSprite();
+    } 
+
+        
+        void Update(){
+            moleculeRB.velocity = new Vector2 (moleculeRB.velocity.x,velocity);
+    }
+
+    public void changeSprite(){
+        int x = Random.Range (1, 38);
+        myRenderer.sprite = atlas.GetSprite ("molecula_" + x);
+
+
+    }
+ 
+
+    public void moveLeft(){
+        if (moleculePosition.position.x > -posX/2){
+            moleculePosition.position = new Vector3 (moleculePosition.position.x - posX, moleculePosition.position.y, moleculePosition.position.z);
+        }
+    }
+     public void moveRight(){
+       if (moleculePosition.position.x < posX/2){
+            moleculePosition.position = new Vector3 (moleculePosition.position.x + posX, moleculePosition.position.y, moleculePosition.position.z);
+        }
+    }
+
+    
+  
+
+
+}
